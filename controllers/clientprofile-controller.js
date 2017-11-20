6263347688f46788f714a3ef56b39e6047087d8b
Paul Gelot <pgelot@gmail.com>
2017-11-20 16:01:38 -0500

    client side routes and controller

---
+++ controllers/clientprofile-controller.js
@@ -0,0 +1,75 @@
+
+const clientProfile = require('../models/BizProfile');
+
+
+const clientProfileController = {};
+
+clientProfileController.index = (req, res, next) => {
+  clientProfile.findAll()
+    .then(clients => {
+      res.json({
+        message: 'ok',
+        clients: { clients },
+      });
+    }).catch(next)
+};
+
+clientProfileController.show = (req, res, next) => {
+  clientProfile.findById(req.params.id)
+    .then(user => {
+      res.json({
+        message: 'ok',
+        client: { client },
+      });
+    }).catch(next);
+};
+
+clientProfileController.create = (req, res, next) => {
+  clientProfile.create({
+    age: req.body.bizname,
+    sex: req.body.sex,
+    height: req.body.height,
+    weight: req.body.weight,
+    income: req.body.income,
+    street_address: req.body.street_address,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    user_id: req.body.user_id
+  }).then(biz => {
+    res.json({
+      message: 'User Profile added!',
+      client: { client },
+    });
+  }).catch(next);
+};
+
+clientProfileController.update = (req, res, next) => {
+  clientProfile.update({
+    bizname: req.body.bizname,
+    street_address: req.body.street_address,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    biz_description: req.body.biz_description,
+    biz_url: req.body.biz_url,
+  }, req.params.id).then(biz => {
+    res.json({
+      message: 'User Profile updated successfully!',
+      client: { client },
+    });
+  }).catch(next);
+};
+
+clientProfileController.delete = (req, res, next) => {
+  clientProfile.destroy(req.params.id)
+    .then(() => {
+      res.status(202).json({
+        message: 'User Profile successfully deleted',
+      });
+    }).catch(next);
+}
+
+
+
+module.exports = bizProfileController;
