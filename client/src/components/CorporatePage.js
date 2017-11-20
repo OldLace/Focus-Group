8947fc566c8472fc7296b0a623d0af1ba86f2822
Dylan Moylan <dylanmoylan1@gmail.com>
2017-11-20 15:28:36 -0500

    Adds get groups to corporate page

--- client/src/components/CorporatePage.js
+++ client/src/components/CorporatePage.js
@@ -1,11 +1,54 @@
 import React from 'react'
 
-function CorporatePage(props) {
-  return (
-   <div>
-    Placeholder - main user page for corporate accounts.
-   </div>
-  )
+class CorporatePage extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      groups:null
+    }
+    this.addToGroup = this.addToGroup.bind(this)
+  }
+
+  componentDidMount() {
+    fetch(`/api/biz-routes/`)
+    .then(res => res.json())
+    .then((res) => {
+      this.setState({
+        groups: res.data.groups
+      })
+    })
+    .catch(err => console.log(err))
+  }
+
+  addToGroup() {
+
+  }
+
+  render() {
+    return (
+      <div className="corporate-page">
+        {this.state.groups ?
+          this.state.groups.map(function(el){
+            return (
+              <div className="focus-group">
+                <ul>
+                {el.map(function(el, index) {
+                  return <li key={index}>el.userName</li>
+                })}
+                </ul>
+              </div>
+            )
+          }) :
+          <div className="focus-group">
+            <ul>
+              <li>No Groups found</li>
+            </ul>
+          </div>
+        }
+      </div>
+    )
+  }
 }
 
 export default CorporatePage
