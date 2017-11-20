768fef410c67d78cd7586a33983ed42eb723ede8
Kareem James <kareemdjames@gmail.com>
2017-11-20 09:04:03 -0500

    completed the initial databse structure

--- db/migrations/migration-202611182017.sql
+++ db/migrations/migration-202611182017.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS users (
   firstname VARCHAR(255) NOT NULL,
   lastname VARCHAR(255) NOT NULL,
   username VARCHAR(255) UNIQUE NOT NULL,
---  password_digest TEXT NOT NULL,
+  password_digest TEXT NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL
---  company BOOL
+  company BOOL
 );
 
 CREATE TABLE IF NOT EXISTS user_profiles (
