77baae3903d7c5e72e0838c026456c756eac0620
Kareem James <kareemdjames@gmail.com>
2017-11-28 09:08:31 -0500

    added flexbox and minor css changes to biz logged in page, fixed some structure on css file

--- client/src/components/corporate/BizInfo.js
+++ client/src/components/corporate/BizInfo.js
@@ -3,20 +3,22 @@ import React from 'react'
 function BizInfo(props) {
   return (
     <div className="biz-info">
-      <h1>Business Details</h1>
-      <div>
-        <ul>
-          <li>Business Name: {props.bizDetails.bizname}</li>
-          <li>Street Address: {props.bizDetails.street_address}</li>
-          <li>City: {props.bizDetails.city}</li>
-          <li>State: {props.bizDetails.state}</li>
-          <li>Zipcode: {props.bizDetails.zip}</li>
-          <li>Company URL: {props.bizDetails.biz_url}</li>
-          <li>Business description: {props.bizDetails.biz_description}</li>
-        </ul>
+      <div className="biz-account-details">
+        <h1>Account Details</h1>
+      </div>
+      <div className="biz-details-wrap">
+        <div className="photo"></div>
+        <div className="biz-details">
+          <p>Business Name: {props.bizDetails.bizname}</p>
+          <p>Street Address: {props.bizDetails.street_address}</p>
+        </div>
+        <div className="edit-button">
+          <button>Profile Settings</button>
+        </div>
       </div>
     </div>
   )
 }
 
 export default BizInfo
+
